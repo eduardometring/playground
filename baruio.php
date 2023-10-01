888e3fcaaf33a86651e03e0e6f1248b4b05623ad
Eduardo Metring <67509355+eduardometring@users.noreply.github.com>
2023-10-01 19:06:49 +0000

    Consertando erros

--- baruio.php
+++ baruio.php
@@ -10,7 +10,15 @@
     
 </head>
 <body>
- 
+<?php
+$path = "/testweb/home.php";
+
+//Show filename
+echo basename($path) ."<br/>";
+
+//Show filename, but cut off file extension for ".php" files
+echo basename($path,".php");
+?>
     <script src="/js/som.js"></script>
 </body>
 </html>
