7935ef68e36157e322d1e8a8f3c30a2b582bd659
Eduardo Metring <67509355+eduardometring@users.noreply.github.com>
2023-10-01 17:21:25 +0000

    Script

--- main.js
+++ main.js
@@ -1,33 +1,10 @@
-alert('polayground do japones');
-function tocaSom (seletorAudio) {
-    const elemento = document.querySelector(seletorAudio);
+let age = prompt('Antes de mais nada, responda a seguinte pergunta: \nQuantos anos você tem?');
 
-    if (elemento && elemento.localName === 'audio') {
-        elemento.play();
-    }
-    else {
-        console.log('Elemento não encontrado ou seletor inválido, palhaço');
-    }
-}
+if(age < 13) {
+    alert('Você não tem idade para acessar o Playground do Japonês');
+    window.location.replace('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
-
-for (let contador = 0; contador < listaDeTeclas.length; contador++) {
-    const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`;
-
-    tecla.onclick = function() {
-        tocaSom(idAudio);
-    }
-    tecla.onkeydown = function (evento) {
-        if (evento.code === 'Space' || evento.code === 'Enter') {
-            tecla.classList.add('ativa');
-
-        }
-    }
-
-    tecla.onkeyup = function() {
-        tecla.classList.remove('ativa');
-    }
-}
+} else {
+    let person = prompt('Seja muito bem-vindo(a) ao Playground do Japonês!! \nAntes de mais nada, me diga: \nComo você se chama?');
+    alert('Que nome bacana. Divirta-se, ' + person + '!')
+};
