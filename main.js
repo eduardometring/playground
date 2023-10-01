a3fb6cb6acc5e3d171d9164e46a988b214428e42
Eduardo Metring <67509355+eduardometring@users.noreply.github.com>
2023-10-01 17:38:27 +0000

    Restrição de idade

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 let age = prompt('Antes de mais nada, responda a seguinte pergunta: \nQuantos anos você tem?');
 
-if(age < 13) {
+ if(age < 13) {
     alert('Você não tem idade para acessar o Playground do Japonês');
     window.location.replace('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
 
 } else {
-    let person = prompt('Seja muito bem-vindo(a) ao Playground do Japonês!! \nAntes de mais nada, me diga: \nComo você se chama?');
-    alert('Que nome bacana. Divirta-se, ' + person + '!')
+    alert('Seja muito bem vindo(a) ao Playground do Japonês!!')
 };
